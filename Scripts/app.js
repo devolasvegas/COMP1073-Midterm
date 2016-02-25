f95635b8671a87ebd93aa55167bf9cec3f0cf9e0
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-02-25 09:33:22 -0500

    Add DOM node variables to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,11 +1,25 @@
 (function(){
-var travelReport = function () {
+
+    // Variables to store DOM nodes for content injection
+    var introParagraph = document.getElementById('IntroParagraph');
+    var folegandros = document.getElementById('Folegandros');
+    var alonissos = document.getElementById('Alonissos');
+    var spetses = document.getElementById('Spetses');
+    var amorgos = document.getElementById('Amorgos');
+    var syros = document.getElementById('Syros');
+    var milos = document.getElementById('Milos';)
+    var hydra = document.getElementById('Hydra';)
+    var ithaca = document.getElementById('Ithaca';)
+    var gavdos = document.getElementById('Gavdos';)
+
+
+    var travelReport = function () {
 
     "use strict";
     console.log("App Started...")
 
 
-}
+    }
 
 })();
 
